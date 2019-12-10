a246b2473c7ae0a3ba746159b56c39b470e1c097
MichaelPoillot <michaelkristyan@gmail.com>
2019-12-10 20:57:28 +0700

    membuat view pagger

---
+++ app/src/main/java/com/example/mentalilness/pagelogin.java
@@ -0,0 +1,14 @@
+package com.example.mentalilness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pagelogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagelogin);
+    }
+}
