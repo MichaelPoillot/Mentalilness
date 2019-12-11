e031dac9a69204aacded431a8c8a4d5a7d772690
MichaelPoillot <michaelkristyan@gmail.com>
2019-12-11 19:11:14 +0700

    membuat bak end view pager

--- app/src/main/java/com/example/mentalilness/splashscreen.java
+++ app/src/main/java/com/example/mentalilness/splashscreen.java
@@ -16,7 +16,7 @@ public class splashscreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(splashscreen.this,MainActivity.class));
+                startActivity(new Intent(splashscreen.this,welcomeactivity.class));
                 finish();
             }
         },2000);
