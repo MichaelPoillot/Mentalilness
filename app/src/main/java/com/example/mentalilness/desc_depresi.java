70c7cafc81861d91b19e9be23343bf2744ff39bb
MichaelPoillot <michaelkristyan@gmail.com>
2019-12-12 17:38:58 +0700

    membuat main activity

---
+++ app/src/main/java/com/example/mentalilness/desc_depresi.java
@@ -0,0 +1,14 @@
+package com.example.mentalilness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class desc_depresi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desc_depresi);
+    }
+}
