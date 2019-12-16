c586b38e68cc0a9995c572306f7199e64c2f13ce
Damas Andrean <andreandamas@gmail.com>
2019-12-16 17:34:20 +0700

    desc kepercayaan diri fix (Pinjam komputer damas)

--- app/src/main/java/com/example/mentalilness/desc_kepercayaandiri.java
+++ app/src/main/java/com/example/mentalilness/desc_kepercayaandiri.java
@@ -2,7 +2,9 @@ package com.example.mentalilness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class desc_kepercayaandiri extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class desc_kepercayaandiri extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desc_kepercayaandiri);
+    }
+
+    public void closekepercayaandiri(View view) {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
